345db36a294bb3f09b291773b28118814ed90107
Cygnet717 <kathybradt717@gmail.com>
2021-11-19 10:51:29 -0600

    basic layout of contact page and resume page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react';
+import Form from 'react-bootstrap/Form'
 
-const Contacts = () => {
+const Contact = () => {
 
-  useEffect(() => {
-    
-  }, []);
 
   return (
     <div className="">
-      <h1>Contacts</h1>
+      <h1>Contact</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
     </div>
   );
 };
 
-export default Contacts;
+export default Contact;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,7 +8,22 @@ const Resume = () => {
 
   return (
     <div className="">
-      
+      <h3>Education</h3>
+      <div>
+        <h4>U of M Bootcamp</h4>
+        <p>Completed December 2021</p>
+        <p>Fast paced, indepth, full stack web development course</p>
+      </div>
+      <div>
+        <h4>Thinkful</h4>
+        <p>Completed February 2020</p>
+        <p>Self-paced program working with a mentor to learn full stack web development</p>
+        <p>Pandemic interfiered with the job hunt.</p>
+      </div>
+      <h3>Work History</h3>
+      <div>
+        <h4>Certified Veteranary Technician</h4>
+      </div>
     </div>
   );
 };
