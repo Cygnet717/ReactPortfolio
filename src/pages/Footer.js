4b445fa54b3a74f1a201dc779f92fee73e5708b7
Cygnet717 <kathybradt717@gmail.com>
2021-12-06 10:27:04 -0600

    added finsing touches

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,4 +1,5 @@
 import Button from 'react-bootstrap/Button'
+import resume from './resume/Kathy Bradt-Resume.pdf'
 const Footer = () => {
   const styles = {
     footer: {
@@ -20,9 +21,10 @@ const Footer = () => {
   }
   return (
     <div className="" style={styles.footer}>
-      <Button style={styles.link} href='https://github.com/Cygnet717' variant="outline-dark">Git Hub</Button>
-      <Button style={styles.link} href='https://www.linkedin.com/in/kathy-bradt-1796a9191/' variant="outline-dark">Linked In</Button>
-      <Button style={styles.link} href='https://stackoverflow.com/users/13035422/kathyb' variant="outline-dark">Stack Overflow</Button>
+      <Button style={styles.link} target="_blank" href='https://github.com/Cygnet717' variant="outline-dark">Git Hub</Button>
+      <Button style={styles.link} target="_blank" href='https://www.linkedin.com/in/kathy-bradt-1796a9191/' variant="outline-dark">Linked In</Button>
+      <Button style={styles.link} target="_blank" href='https://stackoverflow.com/users/13035422/kathyb' variant="outline-dark">Stack Overflow</Button>
+      <Button style={styles.link} target="_blank" href={resume} variant="outline-dark">Resume</Button>
     </div>
   );
 };
